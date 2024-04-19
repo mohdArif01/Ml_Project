6287de6446644a8a05e21b13fb89c2d0a2854484
mohdArif01 <mohdarif.14011@gmail.com>
2024-04-19 17:27:51 +0530

    The logger and Exception files has been initialised

---
+++ app.py
@@ -0,0 +1,12 @@
+from src.mlproject.exception import CustomException
+from src.mlproject.logger import logging
+import sys
+
+if __name__=="__main__":
+    logging.info("The execution has started")
+
+    try:
+        a = 1/0
+    except Exception as e:
+        logging.info("Custom Exception")
+        raise CustomException(e,sys)
---
+++ src/mlproject/logger.py
@@ -0,0 +1,15 @@
+import logging
+import os
+from datetime import datetime
+
+LOG_FILE = f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
+log_path = os.path.join(os.getcwd(), "logs", LOG_FILE)
+os.makedirs(log_path, exist_ok=True) # exist_ok=True means is the file is already present it skips
+
+LOG_FILE_PATH = os.path.join(log_path, LOG_FILE) # Complete path of log file
+
+logging.basicConfig(
+    filename=LOG_FILE_PATH,
+    format="[ %(asctime)s ] %(lineno)d %(name)s - %(levelname)s - %(message)s",
+    level=logging.INFO,
+)
